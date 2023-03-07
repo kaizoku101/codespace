ef4bd5429c5d3d21280cdefd32cbee071080fcc2
kaizoku101 <113707308+kaizoku101@users.noreply.github.com>
2023-03-07 07:30:48 +0000

    Tue, Mar 7, 2023, 7:30 AM +00:00

--- search.c
+++ search.c
@@ -3,6 +3,18 @@
 
 int main (void)
 {
-    int numbers [] = {20,45,59,70,100,101};
-    int n = get_int
+    int numbers [] = {20,45,59,70,100,101,90};
+    int n = get_int("number");
+    for(int i=0 ; i<7; i++)
+    {
+        if(numbers==n)
+        {
+            printf("number found");
+            return 0;
+        }
+        else
+        {
+            printf("not found");
+        }
+    }
 }
